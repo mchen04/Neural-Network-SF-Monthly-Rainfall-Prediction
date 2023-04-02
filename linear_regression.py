982b751dce4f65531da1d2d8c244876ad7c6f666
mchen04 <michaelluochen1@gmail.com>
2023-04-01 22:37:09 -0700

    Linear Regression Completed

--- linear_regression.py
+++ linear_regression.py
@@ -2,29 +2,60 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 # Load the dataset into a DataFrame
 df = pd.read_csv('modified_dataset_file.csv')
 
 # Split the data into features and labels
-labels = df.iloc[:, 1:].values.reshape(-1, 12)   # rainfall values for each month
-features = df.iloc[:, 0].values.reshape(-1, 1)   # year column only
+labels = df.iloc[:, 1:].values
+features = df.iloc[:, 0].values.reshape(-1, 1)
 
-# Train the linear regression model
-model = LinearRegression().fit(features, labels)
+# Calculate the train size as the largest multiple of 12 that is less than or equal to 80% of the length of the dataset
+train_size = len(df) - len(df) % 12
+train_size = int(0.8 * train_size)
 
-# Predict the monthly rainfall for 2023
-year_2023 = np.array([2023]).reshape(-1, 1)
-monthly_predictions_2023 = model.predict(year_2023).flatten()
+# Split the data into training and test sets
+train_features, train_labels = features[:train_size], labels[:train_size]
+test_features, test_labels = features[train_size:], labels[train_size:]
 
-# Print the predicted rainfall for each month in 2023
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-for i, month in enumerate(months):
-    print(f"{month}: {monthly_predictions_2023[i]:.2f} inches")
+# Initialize a dictionary to store the trained models for each month
+models = {}
 
-# Plot the predicted rainfall for each month in 2023
-plt.plot(months, monthly_predictions_2023)
-plt.xlabel('Month')
-plt.ylabel('Rainfall (inches)')
-plt.title('Predicted Rainfall for 2023')
+# Train a separate linear regression model for each month
+for month in range(12):
+    model = LinearRegression()
+    model.fit(train_features, train_labels[:, month])
+    models[month] = model
+
+# Use the trained models to make predictions on the test data
+test_predictions = np.zeros_like(test_labels)
+for month in range(12):
+    model = models[month]
+    test_predictions[:, month] = model.predict(test_features)
+
+# Compute the mean squared error between the predicted and actual rainfall values for each month
+mse = mean_squared_error(test_labels, test_predictions, multioutput='raw_values')
+for month, error in enumerate(mse):
+    print(f"Mean Squared Error for Month {month + 1}: {error:.2f} inches")
+
+# Create a figure with subplots for each month
+fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(12, 9))
+
+# Loop over each month and create a scatter plot on the corresponding subplot
+for month in range(12):
+    row = month // 4
+    col = month % 4
+    ax = axs[row][col]
+    ax.plot(test_features, test_labels[:, month], label='Actual', color='blue')
+    ax.plot(test_features, test_predictions[:, month], label='Predicted', color='orange')
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Rainfall (inches)")
+    ax.set_title(f"Month {month + 1}")
+    ax.legend()
+
+# Adjust the spacing between the subplots
+plt.subplots_adjust(wspace=0.3, hspace=0.5)
+
+# Show the figure
 plt.show()
