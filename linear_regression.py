66846ddbe01da599096666d87d44eaa730af28a8
mchen04 <michaelluochen1@gmail.com>
2023-04-01 17:08:56 -0700

    2023 Predictions Made

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Load the dataset into a DataFrame
@@ -12,8 +13,18 @@ features = df.iloc[:, 0].values.reshape(-1, 1)   # year column only
 # Train the linear regression model
 model = LinearRegression().fit(features, labels)
 
-# Make a prediction for a new set of features
-new_features = np.array([[2023]])
-prediction = model.predict(new_features)
+# Predict the monthly rainfall for 2023
+year_2023 = np.array([2023]).reshape(-1, 1)
+monthly_predictions_2023 = model.predict(year_2023).flatten()
 
-print(prediction)
+# Print the predicted rainfall for each month in 2023
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+for i, month in enumerate(months):
+    print(f"{month}: {monthly_predictions_2023[i]:.2f} inches")
+
+# Plot the predicted rainfall for each month in 2023
+plt.plot(months, monthly_predictions_2023)
+plt.xlabel('Month')
+plt.ylabel('Rainfall (inches)')
+plt.title('Predicted Rainfall for 2023')
+plt.show()
